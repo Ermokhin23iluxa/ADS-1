5da91e1e248c97311219613a48f3fe4952c1a22b
Ermokhin23iluxa <126194668+Ermokhin23iluxa@users.noreply.github.com>
2023-03-07 14:32:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,35 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  int cot = 0;
+  if (value == 1) return 0;
+  for (int i = 1; i < (value / 2) + 1; i++)
+    if (value%i == 0) cot++;
+  if (cot>1) return false;
+  else return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  int cot = 0;
+  for (int i = 2; i < 100000; i++) {
+    if (checkPrime(i)) {
+      cot++;
+    }
+    if (cot == n) {
+	    return i;
+    }
+  }
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  if (checkPrime(value)) return value;
+  for (int i = value+1; i < value*value*value; i++)
+    if (checkPrime(i)) return i;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  int sum = 0;
+  for (int i = 2; i < hbound; i++)
+    if (checkPrime(i)) sum += i;
+  return sum;
 }
