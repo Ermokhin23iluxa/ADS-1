1d50eabd97be49acc6abceb0d18cde6ebd23855a
Ermokhin23iluxa <126194668+Ermokhin23iluxa@users.noreply.github.com>
2023-03-07 16:42:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,12 +5,20 @@
 
 bool checkPrime(uint64_t value) {
   int cot = 0;
-  if (value == 1) return 0;
-  for (int i = 1; i < (value / 2) + 1; i++)
-    if (value%i == 0) cot++;
-  if (cot > 1) return false;
-  else
+  if (value == 1) {
+    return 0;
+  }
+  for (int i = 1; i < (value / 2) + 1; i++) {
+    if (value%i == 0) {
+      cot++;
+    }
+  }
+  if (cot > 1) {
+    return false;
+  }
+  else {
     return true;
+  }
 }
 
 uint64_t nPrime(uint64_t n) {
@@ -26,14 +34,22 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-  if (checkPrime(value)) return value;
-  for (int i = value+1; i < value*value*value; i++)
-    if (checkPrime(i)) return i;
+  if (checkPrime(value)) {
+    return value;
+  }
+  for (int i = value+1; i < value*value*value; i++) {
+    if (checkPrime(i)) {
+      return i;
+    }
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
   int sum = 0;
-  for (int i = 2; i < hbound; i++)
-    if (checkPrime(i)) sum += i;
+  for (int i = 2; i < hbound; i++) {
+    if (checkPrime(i)) {
+      sum += i;
+    }
+  }
   return sum;
 }
